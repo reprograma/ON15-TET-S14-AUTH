00526593597c23189d707ad696a38cffc5ff8662
brensmiranda <brendah.mirandaa@hotmail.com>
2022-06-25 16:54:24 -0300

    Reprograma Semana 14

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,43 @@
 const UserSchema = require('../models/userSchema');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
 
 const login = (req, res) => {
-    
+    try {
+        UserSchema.findOne({ email: req.body.email }, (error, user) => {
+            console.log("Usuário", user)
+
+            if (!user) {
+                return res.status(401).send({
+                    message: "User não encontrado.",
+                    email: `${req.body.email}`
+                })
+            }
+
+            const validPassword = bcrypt.compareSync(req.body.password, user.password);
+            console.log("A senha é válida?", validPassword)
+            if (!validPassword) {
+                return res.status(401).send({
+                    message: "Login não autorizado!"
+                })
+
+            }
+            ////jwt.sign(nome do usuário, SEGREDO)
+            const token = jwt.sign({ name: user.name }, SECRET);
+            console.log("TOKEN CRIADO", token);
+
+            res.status(200).send({
+                "message": "Login autorizado",
+                token
+                
+            })
+
+        })
+
+    } catch (error) {
+        console.error(error)
+    }
 };
 
 module.exports = {
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,64 @@
-const UserSchema = require("../models/userSchema");
+const userSchema = require("../models/userSchema");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
+const SECRET = process.env.SECRET;
+
+
 
 const getAll = async (req, res) => {
   UserSchema.find(function (err, users) {
-    if(err) {
+    if (err) {
       res.status(500).send({ message: err.message })
     }
-      res.status(200).send(users)
-  }) 
+    res.status(200).send(users)
+  })
 };
 
+
+
+const creatUser = async (req, res) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+  req.body.password = hashedPassword
+
+  try {
+
+    const newUser = new userSchema({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      createdAt: new Date()
+
+
+    })
+
+
+    const savedUser = await newUser.save();
+
+    if (savedUser) {
+      res.status(201).send({
+
+        "message": "User criado com sucesso! :)",
+
+
+        savedUser
+
+      })
+
+    }
+
+  } catch (error) {
+    console.error(error);
+
+  }
+
+}
+
+
+
+
+
 module.exports = {
-  getAll
+  getAll,
+  creatUser
 };
