47ace6e736031602f0e46b4706703211e58e234b
natalia2506 <natalia_cmoura@hotmail.com>
2022-06-30 17:52:22 -0300

    Autenticação e token

---
+++ para _o_lar/src/controller/authController.js
@@ -0,0 +1,47 @@
+const UserSchema = require('../model/userSchema');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const SECRET = process.env.SECRET;
+
+
+const login = (req, res) => {
+    try {
+        
+        UserSchema.findOne({ email: req.body.email }, (error, user) => {
+            console.log("USUÁRIO", user)
+            if(!user) {
+                return res.status(401).send({
+                    message: "User não encontrado",
+                    email: `${req.body.email}`
+                })
+            }
+
+           
+            const validPassword = bcrypt.compareSync(req.body.password, user.password);
+            console.log("A SENHA É VÁLIDA?", validPassword)
+
+            if(!validPassword) {
+                return res.status(401).send({
+                    "message": "Login não autorizado",
+                    "statusCode": 401
+                })
+            }
+
+            
+            const token = jwt.sign({name: user.name}, SECRET);
+            console.log("TOKEN CRIADO:", token);
+
+            res.status(200).send({
+                "message": "Login autorizado",
+                token
+            });
+        })
+    } catch(err) {
+        console.error(err)
+    }
+};
+
+module.exports = {
+    login
+};
--- para _o_lar/src/routes/userRoutes.js
+++ para _o_lar/src/routes/userRoutes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controller/userController");
+const authController = require("../controller/authController");
 
 router.get("/all", controller.getAll);
 
 router.post("/create", controller.createUser);
+
+router.post('/login', authController.login);
 
 module.exports = router;
