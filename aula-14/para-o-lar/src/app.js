e7588bd20d09312ed5be034360c2a1d2d47a2ad5
Alice_Lins <alicelins17@yahoo.com>
2022-06-30 19:14:20 -0300

    instalando as dependencias

---
+++ aula-14/para-o-lar/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv-safe').config();
+
+const db = require('./Config/dataBase');
+//const admRoutes = require('./Routes/admRoutes');
+
+db.connect() ;
+
+app.use(cors());
+app.use(express.json());
+//app.use("/adm", admRoutes);
+
+module.exports = app;
