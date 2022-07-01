c25a549fc4c8f64ae49ea9f6a419679fa767dbae
elizapimentel <elizapimentel@hotmail.com>
2022-07-01 20:56:49 -0300

    :sparkles: add delete by id

--- para-o-lar/src/routes/ordersRouts.js
+++ para-o-lar/src/routes/ordersRouts.js
@@ -6,7 +6,7 @@ router.post("/create", controller.createOrder)
 router.get("/all", controller.getAllOrders)
 router.get("/id/:id", controller.findById)
 router.put("/update/:id", controller.updateOrder)
-
+router.delete("/delete/:id", controller.deleteOrder)
 
 
 module.exports = router
