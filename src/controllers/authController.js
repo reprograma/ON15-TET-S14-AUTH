27fd02eff4f03a916dcf7ae3eb664309af59d1f7
Elenfrancyne <elenfrancyne@gmail.com>
2022-07-01 15:02:16 -0300

    Uma chaves fora de lugar

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,8 +5,8 @@ const jwt = require("jsonwebtoken")
 const SECRET = process.env.SECRET;
 const login = (req, res) => {
     try {
-        UserSchema.findOne({ email: req.body.email }, (error, user) => {
-
+        UserSchema.findOne({ email: req.body.email }), (error, user) => {
+            console.log("USUÁRIO", user)
             if (!user) {
                 return res.status(401).send({
                     message: "User não encontrado",
@@ -15,26 +15,25 @@ const login = (req, res) => {
             }
             const validPassword = bcrypt.compareSync(req.body.password, user.password);
             console.log("A SENHA É VÁLIDA?", validPassword);
-            if (!validPassword) {
-                return res.status(401).send({
-                    "message": "Login não autorizado"
-                })
-            };
-            const token = jwt.sing({ name: user.name }, SECRET);
-            console.log("TOKEN CRIADO COM SUCESSO!!!", token)
+            return res.status(401).send({
+                message: "Login não autorizado!"
+            })
+        };
+        const token = jwt.sing({ name: user.name }, SECRET);
+        console.log("TOKEN CRIADO COM SUCESSO!!!", token)
 
-            res.status(200).send({
-                "message": "Login autorizado",
-                token
-            });
+        res.status(200).send({
+            "message": "Login autorizado",
+            token
+        });
 
-        })
 
     } catch (error) {
         console.error(error)
     }
 };
 
+
 module.exports = {
     login
-};
+}
