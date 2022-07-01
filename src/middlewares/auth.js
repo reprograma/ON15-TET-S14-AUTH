e76751f68a5bc9e94050c8fb11fb4efda4893e9a
BrunaCelestino <brunaccelestino@gmail.com>
2022-07-01 02:47:20 -0300

    :boom: - fix - resolvendo conflitos com o repositorio original

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.SECRET;
 
@@ -20,34 +19,3 @@ exports.checkAuth = async (req, res, next) => {
     }catch(erro){
 console.error(erro)
     }}
-=======
-const jwt = require("jsonwebtoken");
-
-const SECRET = process.env.SECRET;
-
-exports.checkAuth = (req, res, next) => {
-    const authHeader = req.get('authorization')
-    console.log("AUTH HEADER", authHeader)
-    const token = authHeader.split(' ')[1];
-    console.log("TOKEN", token)
-    
-    if (!token) {
-      return res.status(401).send("Erro no header")
-    }
-
-    try {
-        jwt.verify(token, SECRET, (err) => {
-            if(err) {
-                return res.status(401).send("Não autorizado")
-            }
-        });
-
-        next();
-          
-    } catch(err) {
-        console.error(err);
-    }
-}
-
-// module.exports = { checkAuth }
->>>>>>> 3a17d9db4b2b11e889c721545fea9312b22c1e6a
