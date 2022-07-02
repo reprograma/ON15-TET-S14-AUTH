1b4e227d57a7d90cd1723941d04f115bfd976752
paola-rodrigues <paola.n.rodrigues@gmail.com>
2022-07-01 23:56:53 -0300

    Subindo a atividade 14

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const UserSchema = require("../models/userSchema");
+const bcrypt = require('bcrypt');
+
 
 const getAll = async (req, res) => {
   UserSchema.find(function (err, users) {
@@ -9,6 +11,60 @@ const getAll = async (req, res) => {
   }) 
 };
 
-module.exports = {
-  getAll
+const createUser = async (req, res) =>{
+  try {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword
+
+    if(!req.body.name || !req.body.email || !req.body.password){
+      res.status(404).send({
+        "message": "Os campos obrigatórios precisam ser enviados",
+        "statusCode": 404
+      })
+    }
+
+    const newUser = new UserSchema({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+
+    });
+    const saverUser = await newUser.save();
+    if(saverUser) {
+      res.status(201).send({
+        "message": "Nota criada com sucesso",
+        saverUser
+      })
+    }
+    
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({
+      message: e.message
+    })
+
+  };
 };
+
+module.exports = {
+  getAll,
+  createUser
+};
+
+
+
+// name: {
+//   type: String,
+//   required: true
+// },
+// email: {
+//   type: String,
+//   required: true
+// },
+// password: {
+//   type: String,
+//   required: false
+// },
+// createdAt: {
+//   type: Date,
+//   default: new Date()
