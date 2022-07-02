ebe671743c0e0813abf510656bda099535a33057
Rosana30 <rosanasouza2@hotmail.com>
2022-07-01 22:39:35 -0300

    Atividade da Semana 14 autenticação / autorização

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,10 @@
 const UserSchema = require("../models/userSchema");
+const bcrypt = require('bcrypt');
+
 
 const getAll = async (req, res) => {
+
+
   UserSchema.find(function (err, users) {
     if(err) {
       res.status(500).send({ message: err.message })
@@ -9,6 +13,35 @@ const getAll = async (req, res) => {
   }) 
 };
 
-module.exports = {
-  getAll
+const createUser = async (req, res) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+  req.body.password = hashedPassword;
+  //"senhadifícil" = "
+
+  
+
+ 
+  try {
+   
+    const newUser = new UserSchema({ name: req.body.name, 
+        email: req.body.email, 
+        password: req.body.password
+      });
+
+    const savedUser = await newUser.save();
+
+      res.status(201).send({
+        "message": "User criado com sucesso",
+        savedUser
+      });
+
+  } catch(e) {
+    console.error(e);
+  };
 };
+
+
+module.exports = {
+  getAll,createUser
+};
+
